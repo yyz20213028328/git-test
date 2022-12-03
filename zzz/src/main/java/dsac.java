270091aa639edf2e1ff146cb6d0df518fcf38eb1
yyz <yyz@qq.com>
2022-12-03 17:44:25 +0800

    first commit

---
+++ zzz/src/main/java/dsac.java
@@ -0,0 +1,11 @@
+/**
+ * dsac
+ *
+ * @author ${user}
+ * @date 2022-12-03 17:32
+ */
+public class dsac {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
---
+++ zzz/src/main/java/hk.java
@@ -0,0 +1,8 @@
+/**
+ * hk
+ *
+ * @author ${user}
+ * @date 2022-12-03 17:38
+ */
+public class hk {
+}
