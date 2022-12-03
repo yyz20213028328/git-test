d642659fe8e0be8f701e93e0b52479b20f7f2f1b
yyz <yyz@qq.com>
2022-12-03 18:58:14 +0800

    push commit

--- zzz/src/main/java/dsac.java
+++ zzz/src/main/java/dsac.java
@@ -12,5 +12,6 @@ public class dsac {
         System.out.println("git3");
         System.out.println("master test");
         System.out.println("git hot-fix test");
+        System.out.println("push test");
     }
 }
